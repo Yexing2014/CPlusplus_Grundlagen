b00546dc393aaaac75f81ab572699d5a23ebb0c7
Yexing Geng <gengyexing19901007@gmail.com>
2022-09-21 17:42:11 +0200

    new übung

---
+++ Kapitel01/Kapitel01/Übung1.16.cpp
@@ -0,0 +1,23 @@
+﻿// Aufgabe: Übung 1.16
+// Author: Yexing Geng
+// Datum: 21.09.2022
+
+#include<iostream>
+
+int main()
+{
+	// init 
+	int num = 0;
+	int sum = 0;
+
+	std::cout << "Geben Sie die gewünschte Zahle ein: " << std::endl;
+
+	while (std::cin >> num)
+	{
+		sum += num;
+	}
+
+	std::cout<< "Sum is: " << sum << std::endl;
+
+	return 0;
+}
